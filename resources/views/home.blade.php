7696f163926ae34c091930ce3ebecab26e89c2e3
Jinky Llones <kyjinllones@gmail.com>
2019-02-01 13:46:24 +0800

    First Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container" >
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            
+                <div class="card ">
+                    <div class="card-header">Add Organization</div>
+
+                    <div class="card-body" >
+                        <label>Organization</label><br/>
+                        <input type="" name=""/><br><br>
+                        <button class="btn btn-primary">Submit</button>
+                    </div>
+                </div>
+                  <div class="card">
+                    <div class="card-header">List Of Organization</div>
+
+                    <div class="card-body">
+                        <table>
+                            <tr>Organization</tr>
+                        </table>
+                       
+                    </div>
+                </div>
+                
+        </div>
+    </div>
+</div>
+@endsection
